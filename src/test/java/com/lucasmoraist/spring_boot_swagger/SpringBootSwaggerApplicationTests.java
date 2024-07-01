6b7dff967dd08fd2a3540c579cab0c954ff064bc
lucasmoraist <luksmnt1101@gmail.com>
2024-07-01 15:37:22 -0300

    feat: esqueleto do projeto

---
+++ src/test/java/com/lucasmoraist/spring_boot_swagger/SpringBootSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.spring_boot_swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
